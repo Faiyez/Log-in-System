5f938973742ddab9f4f2633f7dd2d3c5d2d31e06
Faiyez <smfnoor@mun.ca>
2022-01-31 12:31:13 -0330

    Added buttons

--- loginPage.java
+++ loginPage.java
@@ -1,21 +1,17 @@
 import java.util.HashMap;
-
 import javax.swing.*;
-
-import org.w3c.dom.UserDataHandler;
-
 import java.awt.*;
 import java.awt.event.*;
 
-public class loginPage extends JFrame implements ActionListener{
+public class loginPage extends JFrame implements ActionListener, java.awt.event.ActionListener{
 //    JFrame frame = new JFrame();
-    JButton logIn = new JButton("Login");
+    JButton loginButton = new JButton("Login");
     JButton resetButton = new JButton("Reset");
     JTextField idField = new JTextField();
-    JPasswordField passWordField = new JPasswordField();
+    JPasswordField passwordField = new JPasswordField();
     JLabel loginLabel = new JLabel("Login");
     JLabel passwordLabel = new JLabel("Password");
-    JLabel messageLabel = new JLabel("This is a test");
+    JLabel messageLabel = new JLabel();
     HashMap<String, String> loginInfo = new HashMap<String, String>();
     loginPage(HashMap<String, String> realLoginInfo){
         loginInfo = realLoginInfo;
@@ -23,7 +19,17 @@ public class loginPage extends JFrame implements ActionListener{
         passwordLabel.setBounds(50,150,80,20);
         messageLabel.setBounds(100,250,200,40);
         messageLabel.setFont(new Font(null,Font.BOLD,20));
-        
+        idField.setBounds(120,100,200,20);
+        passwordField.setBounds(120,150,200,20);
+        loginButton.setBounds(140,200,80,20);
+        loginButton.addActionListener(this);
+        resetButton.setBounds(220,200,80,20);
+        resetButton.addActionListener(this);
+
+        add(resetButton);
+        add(loginButton);
+        add(passwordField);
+        add(idField);
         add(messageLabel);
         add(loginLabel);
         add(passwordLabel);
