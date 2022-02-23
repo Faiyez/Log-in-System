4441956bcf6096467a167bd2f1d415c3168b3a06
Faiyez <smfnoor@mun.ca>
2022-02-23 15:24:33 -0330

    Updated Functionalities

--- loginPage.java
+++ loginPage.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class loginPage extends JFrame implements ActionListener, java.awt.event.ActionListener{
+public class loginPage extends JFrame implements ActionListener{
 //    JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
     JButton resetButton = new JButton("Reset");
@@ -12,12 +12,13 @@ public class loginPage extends JFrame implements ActionListener, java.awt.event.
     JLabel loginLabel = new JLabel("Login");
     JLabel passwordLabel = new JLabel("Password");
     JLabel messageLabel = new JLabel();
+    
     HashMap<String, String> loginInfo = new HashMap<String, String>();
     loginPage(HashMap<String, String> realLoginInfo){
         loginInfo = realLoginInfo;
         loginLabel.setBounds(50,100,80,20);
         passwordLabel.setBounds(50,150,80,20);
-        messageLabel.setBounds(100,250,200,40);
+        messageLabel.setBounds(120,250,200,100);
         messageLabel.setFont(new Font(null,Font.BOLD,20));
         idField.setBounds(120,100,200,20);
         passwordField.setBounds(120,150,200,20);
@@ -25,6 +26,7 @@ public class loginPage extends JFrame implements ActionListener, java.awt.event.
         loginButton.addActionListener(this);
         resetButton.setBounds(220,200,80,20);
         resetButton.addActionListener(this);
+        
 
         add(resetButton);
         add(loginButton);
@@ -41,6 +43,29 @@ public class loginPage extends JFrame implements ActionListener, java.awt.event.
 
 
     public void actionPerformed(ActionEvent e){
+        if(e.getSource() == resetButton){
+            idField.setText("");
+            passwordField.setText("");
+        }
+        if(e.getSource() == loginButton){
+            String userName = idField.getText();
+            String userPass = String.valueOf(passwordField.getPassword());
+            if(loginInfo.containsKey(userName)){
+                if(loginInfo.get(userName).equals(userPass)){
+                    messageLabel.setForeground(Color.GREEN);
+                    messageLabel.setText("Log in successfull");
+                }
+                else{
+                    messageLabel.setForeground(Color.RED);
+                    messageLabel.setText("Incorrect Password ");                    
+                }
+            }
+            else{
+                messageLabel.setForeground(Color.RED);
+                messageLabel.setText("Wrong Username");
+            }
+        }
+        
 
     }
 }
